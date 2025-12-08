b52549cf0c5fdad262dfbf677f7d57f7b49c147c
gutti-misdo <sakahaya3@icloud.com>
2025-12-09 02:28:41 +0900

    Unify styling with shared app stylesheet

--- home-page/parts-add.php
+++ home-page/parts-add.php
@@ -64,10 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>アルバイト登録 | IIIKANJIKANRIHYOU</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="./css/parts-add.css" />
+    <link rel="stylesheet" href="./css/app.css" />
 </head>
 
 <body>
